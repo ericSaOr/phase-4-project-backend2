c541f7be607b793fb3db84ee89ea711ae31b5393
Leonna Jones <leonnajones@leonnas-mbp.lan>
2021-09-15 21:16:00 -0400

    cookies work from backend client

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+    # def cookiejar
+    #     puts "HEY PARTY PEOPLE I'M #{self}"
+    #     session[:user_id] = "user.id"
+    #     render plain: "potato"
+    # end
+
+    # def tummy
+    #     puts "HEY ###### #{self}"
+    #     render json: session[:user_id]
+    # end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "first", to: "users#cookiejar"
+  get "second", to: "users#tummy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
